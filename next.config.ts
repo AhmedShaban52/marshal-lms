0fc2e83e58a5045f753d2349f7a7b8ffd0554ff7
Ahmed Shaaban <111895280+AhmedShaban52@users.noreply.github.com>
2025-08-22 16:37:25 +0300

    1

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,25 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import type { NextConfig } from "next";
+// @ts-ignore
+import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
 
 const nextConfig: NextConfig = {
-   images:{
+  images: {
     remotePatterns: [
       {
-        hostname:"marshal-lms-yt.t3.storageapi.dev",
-        port:'',
-        protocol:'https'
-      }
-    ]
-   }
+        hostname: "marshal-lms-yt.t3.storageapi.dev",
+        port: "",
+        protocol: "https",
+      },
+    ],
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()];
+    }
+
+    return config;
+  },
 };
 
 export default nextConfig;
